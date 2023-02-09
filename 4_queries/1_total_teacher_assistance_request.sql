82686a9284bc6a712ecdaa8815d50e35bc2404d1
Abe <abrahamrodgers@gmail.com>
2023-02-08 20:07:11 -0800

    Add JS queries for teachers.js.

--- 4_queries/1_total_teacher_assistance_request.sql
+++ 4_queries/1_total_teacher_assistance_request.sql
@@ -2,4 +2,4 @@ SELECT count(assistance_requests.*) as total_assistances, teachers.name
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
-GROUP BY teachers.name;
+GROUP BY teachers.name; 
